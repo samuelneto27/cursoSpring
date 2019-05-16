e5fdf058d52670be5af46aa33330aa1490156426
Samuel Neto <samudemolay27@gmail.com>
2019-05-16 14:39:11 -0300

    Tratando exceções

--- src/main/java/com/samuel/cursomc/domain/Produto.java
+++ src/main/java/com/samuel/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 @Entity
 
@@ -25,6 +27,8 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
+	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"),
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
@@ -100,4 +104,4 @@ public class Produto implements Serializable {
 	
 	
 
-}
+ }
--- src/main/java/com/samuel/cursomc/services/CategoriaService.java
+++ src/main/java/com/samuel/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.samuel.cursomc.domain.Categoria;
 import com.samuel.cursomc.repository.CategoriaRepository;
+import com.samuel.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional <Categoria> obj = cr.findById(id);
+		
+		if ( obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: "+ id + ", Tipo: "+ Categoria.class.getName());
+		}
 		
 		return obj.orElse(null);
 	}
